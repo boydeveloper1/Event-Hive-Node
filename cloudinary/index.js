71639c97dce7a1ce3c74fb45c4fc0814c13d1f48
Adetayo Chukwudi <adetayo.ca@gmail.com>
2023-10-09 14:15:28 -0400

    changed the storage folder for my cloudinary

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "Mern-Stack",
+    folder: "EventHive",
     allowedFormats: ["jpeg", "jpg", "png"],
   },
 });
